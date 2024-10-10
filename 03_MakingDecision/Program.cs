36dd143e1a533a2bedef00383e638a2b90081cdb
DilekAyvaz <dilekayvaz0@gmail.com>
2024-10-10 21:33:56 +0300

    Proje dosyası ekle.

---
+++ 03_MakingDecision/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_MakingDecision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region if else
+
+            //Console.Write("Lütfen şifreyi giriniz:");
+            //string password;
+            //password = Console.ReadLine();
+            //if (password == "abcd")
+            //{
+            //    Console.WriteLine("Şifre Doğru!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Şifre Yanlış!");
+            //}
+
+            //string capital, country;
+            //Console.Write("Başkenti Giriniz:");
+            //capital = Console.ReadLine();
+            //Console.Write("Ülkeyi Giriniz:");
+            //country = Console.ReadLine();
+
+            //if (capital == "ankara" & country == "türkiye")
+            //{
+            //    Console.Write("veriler doğrulandı.");
+            //}
+
+            //else
+            //{
+            //    Console.Write("hatali bilgi!");
+            //}
+
+            //int number;
+            //Console.Write("sayiyi giriniz:");
+            //number= int.Parse(Console.ReadLine());
+            //if (number == 5)
+            //{
+            //    Console.WriteLine("Sayi doğru");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Sayi hatali");
+            //}
+
+            double exam1, exam2, exam3, average;
+            string result;
+
+
+
+            Console.Read(); 
+            #endregion
+        }
+    }
+}
